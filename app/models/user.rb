f5c0a9d22a5d131caa09e012640a244cb56bfc4a
Marko Zecevic <marczecevic@gmail.com>
2021-05-13 13:54:16 +0200

    Add friendship controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverted_friendships
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
