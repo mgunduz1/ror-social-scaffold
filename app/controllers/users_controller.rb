db474ade70206094027be1bf274262b19fea750d
Marko Zecevic <marczecevic@gmail.com>
2021-05-13 14:51:05 +0200

    Update view to see pending requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @sent_requests = Friendship.where(user_id: current_user.id).where(confirmed: nil)
-    @pending_requests = Friendship.where(friend_id: current_user.id).where(confirmed: nil)
+    @sent = Friendship.where(user_id: current_user.id).where(confirmed: nil)
+    @pending = Friendship.where(friend_id: current_user.id).where(confirmed: nil)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,9 +20,9 @@ module ApplicationHelper
     @check = Friendship.where(user_id: user.id).where(friend_id: current_user.id).first
     return unless user.id != current_user.id && !@friends && !@check && !user.friends.include?(current_user)
 
-    friend = Friendship.where(user_id: current_user.id).where(friend_id: user.id).where(confirmed: nil)
+    friend = Friendship.where(user_id: current_user.id).where(friend_id: user.id).where(confirmed: nil).first
     if friend
-      link_to('Cancel request', friendships_path(friend), class: 'btn btn-outline-danger btn-sm ml-3', method: :delete)
+      link_to('Cancel request', friendship_path(friend), class: 'btn btn-outline-danger btn-sm ml-3', method: :delete)
 
     else
       link_to('Add friend', friendships_path(user), class: 'btn btn-outline-success btn-sm ml-3', method: :post)
@@ -33,5 +33,11 @@ module ApplicationHelper
     friend = Friendship.where(user_id: [current_user.id,user.id]).where(friend_id: [current_user.id,user.id]).where(confirmed: true).first
 
     link_to('Unfriend', friendships_path(friend), class: 'btn btn-outline-danger btn-sm ml-3', method: :delete)
+  end
+
+  def pending_name(user, name)
+    return unless user.id == current_user.id
+
+    name.name
   end
 end
