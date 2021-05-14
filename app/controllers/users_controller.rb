8b6f9efc161032202623ef99eb2501f73847e68f
mgunduz1 <mgunduz114118@gmail.com>
2021-05-14 18:32:21 +0300

    add accept-rejet-cancel logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,8 @@ class UsersController < ApplicationController
     @posts = @user.posts.ordered_by_most_recent
     @sent = Friendship.where(user_id: current_user.id).where(confirmed: nil)
     @pending = Friendship.where(friend_id: current_user.id).where(confirmed: nil)
+    @acceptreject = Friendship.where(friend_id: current_user.id).where(user_id: @user).where(confirmed: nil).first
+    @cancel = Friendship.where(friend_id: @user.id).where(user_id: current_user.id).where(confirmed: nil).first
+    @deletef = Friendship.where(friend_id: @user.id, user_id: current_user.id).or(Friendship.where(friend_id: current_user.id, user_id: @user.id)).where(confirmed: true).first
   end
 end
